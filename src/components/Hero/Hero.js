58c8ed7f1dfedf81df9b115727eb50fa0a64082d
Patrick Karney <patrickkarney@gmail.com>
2021-03-20 18:48:51 +0100

    15.3 Fix the background pic bug

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Hero.scss';
 
+
 class Hero extends React.Component {
     render() {
       return (
         <header className={styles.component}>
             <h2 className={styles.title}>Things to do!</h2>
-            <img className={styles.image} src='space.png'></img>
+            <img className={styles.image} src='https://images.pexels.com/photos/3607542/pexels-photo-3607542.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'></img>
         </header>
         )
     }
